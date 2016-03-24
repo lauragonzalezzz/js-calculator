d17a56f22a1eff71593cd3a842771a592bfd9f9d
Laura <lg3295@gmail.com>
2016-03-23 15:37:46 -1000

    adds calc.js

--- calculator.js
+++ calculator.js
@@ -1,82 +1,81 @@
-/**
- * Declare a function named `calculatorModule`
- * this function will have two private variables declared inside of it.
- * @variable PRIVATE { Number } `memory`
- * @variable PRIVATE { Number } `total`
- * @return {object} `calculator` object that can be used
- */
 
-function calculatorModule() {
-  var _memory;
-  var _total = 0;
-  return {
-    memory : _memory,
-    total : _total,
-    load : function(val){
-      total = val;
-      return total;
-    },
-    getTotal : function() {
-      return total;
+
+var calculatorModule = function (){
+
+    var _total= 0;
+    var _memory= 0;
+
+
+    function calculateTotal(x){
+      if( typeof x !== 'number'){
+        throw new Error('not a number'); 
+      }
+      _total = x;
+      return _total;
     }
+
+    function getTotal(){
+      return _total;
+    }
+
+    function add(x){
+      if( typeof x !== 'number'){
+        throw new Error('not a number'); 
+      }
+      _total += x;
+      return _total;
+    }
+
+    function subtract(x){
+      if( typeof x !== 'number'){
+        throw new Error('not a number'); 
+      }
+      _total = _total-x; 
+      return _total;
+    }
+
+    function multiply(x){
+      if( typeof x !== 'number'){
+        throw new Error('not a number'); 
+      }
+      _total= _total * x;
+      return _total;
+    }
+
+    function divide(x){
+       if( typeof x !== 'number'){
+        throw new Error('not a number'); 
+      }
+      _total= _total/x;
+      return _total; 
+    }
+
+    function recallMemory(){
+      return _memory; 
+    }
+
+    function saveMemory(){
+      _memory = _total;
+      return _memory;
+    }
+
+    function clearMemory(){
+      _memory = 0;
+      return _memory;
+    }
+    
+    return{
+      load: calculateTotal, 
+      getTotal: getTotal,
+      add: add,
+      subtract: subtract,
+      multiply: multiply,
+      divide: divide,
+      recallMemory: recallMemory,
+      saveMemory: saveMemory,
+      clearMemory: clearMemory
+
   };
-}
-var calculator = calculatorModule();
-console.log(calculator.load(903));
-console.log(calculator.getTotal());
-  /**
-   * sets the `total` to the number passed in
-   * @param  { Number } x
-   * @return { Number }    current total
-   */
 
 
-  /**
-   * Return the value of `total`
-   * @return { Number }
-   */
-
-
-  /**
-   * Sums the value passed in with `total`
-   * @param { Number } x
-   */
-
-
-  /**
-   * Subtracts the value passed in from `total`
-   * @param  { Number } x
-   */
-
-
-  /**
-   * Multiplies the value by `total`
-   * @param  { Number } x
-   */
-
-
-  /**
-   * Divides the value passing in by `total`
-   * @param  { Number } x
-   */
-
-
-  /**
-   * Return the value stored at `memory`
-   * @return { Number }
-   */
-
-
-  /**
-   * Stores the value of `total` to `memory`
-   */
-
-
-  /**
-   * Clear the value stored at `memory`
-   */
-
-  /**
-   * Validation
-   */
-
+};
